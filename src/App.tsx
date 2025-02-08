153b265839300b3d43472b3fe12f5f51fee804d0
lzt <1162300237@qq.com>
2025-02-09 01:26:59 +0800

    完成基础样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import FirstSection from './components/FirstSection'
 import SecondSection from './components/SecondSection'
 import ThirdSection from './components/ThirdSection'
+
+import styles from './styles.module.scss'
 function App() {
   return (
-    <>
+    <div className={styles.app}>
       <FirstSection />
       <SecondSection />
       <ThirdSection />
-    </>
+    </div>
   )
 }
 
--- src/components/ThirdSection/index.tsx
+++ src/components/ThirdSection/index.tsx
@@ -8,9 +8,9 @@ const ThirdSection: FC = () => {
   return (
     <div className={styles['third-section']}>
       <img src={Title1Image} alt='Title 1' />
-      <img src={CommentImage} alt='Comment' />
+      <img className={styles.comment} src={CommentImage} alt='Comment' />
       <img src={Title2Image} alt='Title 2' />
-      <img src={CommentImage} alt='Comment' />
+      <img className={styles.comment} src={CommentImage} alt='Comment' />
     </div>
   )
 }
