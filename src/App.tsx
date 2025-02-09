7e89b8cd588f1ebb933a7c406692e5ae453db6b9
lzt <1162300237@qq.com>
2025-02-09 14:31:20 +0800

    feat: 第二部分样式布局

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import FirstSection from './components/FirstSection'
 import SecondSection from './components/SecondSection'
 import ThirdSection from './components/ThirdSection'
 
-import styles from './styles.module.scss'
+import styles from './app.module.scss'
 function App() {
   return (
     <div className={styles.app}>
--- src/components/SecondSection/index.tsx
+++ src/components/SecondSection/index.tsx
@@ -1,4 +1,45 @@
-const SecondSection = () => {
-  return <div className='second-section'></div>
+import { FC } from 'react'
+import CartoonImage from '../../assets/cartoon.jpg'
+import MovieImage from '../../assets/movie.png'
+import FoodImage from '../../assets/food.jpg'
+import LifeImage from '../../assets/life.jpg'
+
+import styles from './index.module.scss'
+
+const SecondSection: FC = () => {
+  return (
+    <div className={styles['second-section']}>
+      {/* tabs */}
+      <ul>
+        <li>动画</li>
+        <li>美食</li>
+        <li>电影</li>
+        <li>生活</li>
+      </ul>
+
+      {/* content */}
+      <div>
+        <section>
+          <h2>动画</h2>
+          <img src={CartoonImage} alt='cartoon' />
+        </section>
+
+        <section>
+          <h2>电影</h2>
+          <img src={MovieImage} alt='movie' />
+        </section>
+
+        <section>
+          <h2>美食</h2>
+          <img src={FoodImage} alt='food' />
+        </section>
+
+        <section>
+          <h2>生活</h2>
+          <img src={LifeImage} alt='life' />
+        </section>
+      </div>
+    </div>
+  )
 }
 export default SecondSection
